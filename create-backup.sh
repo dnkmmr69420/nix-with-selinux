747588d1f1ec7d79d8d4f68d0e21299a26adeae4
dnkmmr <104856296+dnkmmr69420@users.noreply.github.com>
2023-03-24 07:26:27 -0500

    Update create-backup.sh

--- create-backup.sh
+++ create-backup.sh
@@ -9,12 +9,12 @@ sudo cp -R /nix /opt/nb
 
 sudo tee /opt/nb/reset-nix <<EOF
 #!/bin/bash
-sudo echo "Restoring nix..."
+sudo echo "Resettinging nix..."
 sudo rm -rf /nix/*
 sudo mkdir -p /nix
 sudo cp -R /opt/nb/nix/* /nix/
 sudo restorecon -RF /nix
-sudo echo "Nix has been restored. Reboot for changes to apply."
+sudo echo "Nix has been resetted. Reboot for changes to apply."
 EOF
 
 sudo chmod a+x /opt/nb/reset-nix
